1d5ffe2ce34e3297f75742a162680b6084f2f4c1
maoknox <femauro@gmail.com>
2017-06-29 20:53:02 -0500

    prueba 1

--- protected/controllers/ServiceController.php
+++ protected/controllers/ServiceController.php
@@ -141,11 +141,11 @@ class ServiceController extends Controller{
             $magnitudes="";
             foreach($dataFrames as $pk=>$dataFrame){
                 $dataFramesArr= explode(",", $dataFrame->dataframe);
-                print_r($dataFramesArr);exit();
+//                print_r($dataFramesArr);exit();
                 foreach($positionsDF as $pki=>$position){
                     if(is_array($position)){
-                        $pos=3+(int)$position["position_dataframe"];
-                        $magnitudes[$pk][$pki]=$dataFramesArr[$pos];
+                        $pos=(3+(int)$position["position_dataframe"])*1;
+                        $magnitudes[$pk][$pki]=$dataFramesArr[4];
                     }
                 }
                 
