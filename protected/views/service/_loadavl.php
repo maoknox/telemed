2b896fc11b40b18e2247815a68a8246e167fc7b7
maoknox <femauro@gmail.com>
2017-07-07 15:41:49 -0500

    se añade objeto javascript en vista _loadavl

--- protected/views/service/_loadavl.php
+++ protected/views/service/_loadavl.php
@@ -2,6 +2,7 @@
     Yii::app()->clientScript->registerCssFile(Yii::app()->baseUrl.'/plugins/datatables/dataTables.bootstrap.css');
     Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl."/plugins/datatables/jquery.dataTables.min.js",CClientScript::POS_END);
     Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl."/plugins/datatables/dataTables.bootstrap.min.js",CClientScript::POS_END);
+    Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl."/js/Service/Avl.js",CClientScript::POS_END);
 
 
 /* 
